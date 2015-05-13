68c1a81fdde1eaba842965461c95b7ae8dbf2ecb
Patrick Dugan <patrick@nextdoor.com>
2015-05-13 16:37:37 -0700

    Initial Commit

---
+++ codepath-carousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  codepath-carousel
+//
+//  Created by Patrick on 5/13/15.
+//  Copyright (c) 2015 Daily Doog Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ codepath-carouselTests/codepath_carouselTests.swift
@@ -0,0 +1,36 @@
+//
+//  codepath_carouselTests.swift
+//  codepath-carouselTests
+//
+//  Created by Patrick on 5/13/15.
+//  Copyright (c) 2015 Daily Doog Inc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class codepath_carouselTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
