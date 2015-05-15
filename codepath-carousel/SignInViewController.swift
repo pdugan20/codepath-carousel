a2ff96c2b11c3e0f06de97563b157b13af33faac
Patrick Dugan <patrick@nextdoor.com>
2015-05-15 15:59:43 -0700

    updated sign-in form
    improved animation up and down, created animation method

--- codepath-carousel/SignInViewController.swift
+++ codepath-carousel/SignInViewController.swift
@@ -15,10 +15,16 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var signInTextFieldView: UIView!
     @IBOutlet weak var signInButtonView: UIView!
     
+    var initialSignInFormCenter: CGPoint!
+    var initialSignInButtonCenter: CGPoint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        initialSignInFormCenter = signInTextFieldView.center
+        initialSignInButtonCenter = signInButtonView.center
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,15 +42,13 @@ class SignInViewController: UIViewController {
         }
     }
     @IBAction func didBeginEditingEmail(sender: AnyObject) {
+        self.animateUpSignInForm()
         if emailTextField.text == "Email" {
             emailTextField.text = nil
-            
-            UIView.animateWithDuration(0.2, animations: {
-                self.signInTextFieldView.center.y -= 90
-            })
         }
     }
     @IBAction func didBeginEditingPassword(sender: AnyObject) {
+        self.animateUpSignInForm()
         if passwordTextField.text == "Password" {
             passwordTextField.text = nil
         }
@@ -53,6 +57,28 @@ class SignInViewController: UIViewController {
     @IBAction func didEndEditingPassword(sender: AnyObject) {
         if passwordTextField.text.isEmpty {
             passwordTextField.text = "Password"
+        }
+    }
+    
+    @IBAction func didPressSignIn(sender: AnyObject) {
+        
+        // dismisses keyboard from textViews
+        emailTextField.endEditing(true)
+        passwordTextField.endEditing(true)
+        
+        UIView.animateWithDuration(0.4, animations: {
+            self.signInTextFieldView.center = self.initialSignInFormCenter
+            self.signInButtonView.center = self.initialSignInButtonCenter
+        })
+    }
+    
+    func animateUpSignInForm() {
+        // If statement ensures views only animate once
+        if initialSignInFormCenter == signInTextFieldView.center {
+            UIView.animateWithDuration(0.4, animations: {
+                self.signInTextFieldView.center.y -= 90
+                self.signInButtonView.center.y -= 250
+            })
         }
     }
 }
