f15a3db723c55d3c1e1a6b5c32f6432f5e9907df
Patrick Dugan <patrick@nextdoor.com>
2015-05-14 18:51:04 -0700

    sign-in interactions
    disappearing placeholder text and rough animation of form field views

--- codepath-carousel/SignInViewController.swift
+++ codepath-carousel/SignInViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInTextFieldView: UIView!
+    @IBOutlet weak var signInButtonView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,4 +30,29 @@ class SignInViewController: UIViewController {
         navigationController!.popViewControllerAnimated(true)
     }
 
+    @IBAction func didEndEditingEmail(sender: AnyObject) {
+        if emailTextField.text.isEmpty {
+            emailTextField.text = "Email"
+        }
+    }
+    @IBAction func didBeginEditingEmail(sender: AnyObject) {
+        if emailTextField.text == "Email" {
+            emailTextField.text = nil
+            
+            UIView.animateWithDuration(0.2, animations: {
+                self.signInTextFieldView.center.y -= 90
+            })
+        }
+    }
+    @IBAction func didBeginEditingPassword(sender: AnyObject) {
+        if passwordTextField.text == "Password" {
+            passwordTextField.text = nil
+        }
+    }
+    
+    @IBAction func didEndEditingPassword(sender: AnyObject) {
+        if passwordTextField.text.isEmpty {
+            passwordTextField.text = "Password"
+        }
+    }
 }
