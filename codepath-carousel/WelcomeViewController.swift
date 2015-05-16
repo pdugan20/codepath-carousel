ac606cd474d26771b7f6be716731f2f923ad711f
Patrick Dugan <patrick@nextdoor.com>
2015-05-15 17:40:42 -0700

    broken page controller
    page controller sort of works but needs some additional polish

--- codepath-carousel/WelcomeViewController.swift
+++ codepath-carousel/WelcomeViewController.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
+class WelcomeViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        scrollView.delegate = self
+        // scrollView.pagingEnabled = true
+        scrollView.contentSize = CGSize(width: 1280, height: 568)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +27,13 @@ class WelcomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView!) {
+        println("scrolled")
+        // Get the current page based on the scroll offset
+        var page : Int = Int(round(self.scrollView.contentOffset.x / 320))
+        
+        // Set the current page, so the dots will update
+        pageControl.currentPage = page
     }
-    */
 
 }
