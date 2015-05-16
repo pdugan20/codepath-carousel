14a3b6e8cf7abf475ecb6110dc32176c5aace6b5
Patrick Dugan <dugan.pat@gmail.com>
2015-05-16 00:33:47 -0700

    login logic
    added all login logic to login form

--- README.md
+++ README.md
@@ -13,10 +13,10 @@ X.X hours spent in total
 
 **Sign In**
  * [x] Tapping on email/password reveals the keyboard and shifts the scrollview and Sign In button up.
- * [ ] User sees an error alert when no email is present or no password is present.
- * [ ] User sees a loading screen upon tapping the Sign In button.
- * [ ] alertView.dismissWithClickedButtonIndex(0, animated: true) dismisses the loading screen with no buttons.
- * [ ] User sees an error alert when entering the wrong email/password combination.
+ * [x] User sees an error alert when no email is present or no password is present.
+ * [x] User sees a loading screen upon tapping the Sign In button.
+ * [x] alertView.dismissWithClickedButtonIndex(0, animated: true) dismisses the loading screen with no buttons.
+ * [x] User sees an error alert when entering the wrong email/password combination.
  * [x] User is taken to the tutorial screens upon entering the correct email/password combination.
  * [ ] Optional: When the keyboard is visible, if the user pulls down on the scrollview, it will dismiss the keyboard.
  * [x] Optional: On appear, scale the form up and fade it in.
--- codepath-carousel/SignInViewController.swift
+++ codepath-carousel/SignInViewController.swift
@@ -38,6 +38,7 @@ class SignInViewController: UIViewController {
 
     @IBAction func didEndEditingEmail(sender: AnyObject) {
         if emailTextField.text.isEmpty {
+            emailTextField.textColor = UIColor.lightGrayColor()
             emailTextField.text = "Email"
         }
     }
@@ -45,24 +46,35 @@ class SignInViewController: UIViewController {
         self.animateUpSignInForm()
         if emailTextField.text == "Email" {
             emailTextField.text = nil
+            emailTextField.textColor = UIColor.blackColor()
         }
     }
     @IBAction func didBeginEditingPassword(sender: AnyObject) {
         self.animateUpSignInForm()
         if passwordTextField.text == "Password" {
             passwordTextField.text = nil
+            passwordTextField.secureTextEntry = true
+            passwordTextField.textColor = UIColor.blackColor()
         }
     }
     
     @IBAction func didEndEditingPassword(sender: AnyObject) {
         if passwordTextField.text.isEmpty {
+            passwordTextField.secureTextEntry = false
+            passwordTextField.textColor = UIColor.lightGrayColor()
             passwordTextField.text = "Password"
         }
     }
     
     @IBAction func didPressSignIn(sender: AnyObject) {
         
+        var loadingAlert = UIAlertView(title: "Loading...", message: nil, delegate: self, cancelButtonTitle: nil)
+        var emailAlert = UIAlertView(title: "Email Required", message: "Please enter your email address", delegate: self, cancelButtonTitle: "OK")
+        var passwordAlert = UIAlertView(title: "Password Required", message: "Please enter your password", delegate: self, cancelButtonTitle: "OK")
+        var signInAlert = UIAlertView(title: "Sign In Failed", message: "Incorrect email or password", delegate: self, cancelButtonTitle: "OK")
+        
         // dismisses keyboard from textViews
+        // self.view.endEditing(true)
         emailTextField.endEditing(true)
         passwordTextField.endEditing(true)
         
@@ -70,6 +82,24 @@ class SignInViewController: UIViewController {
             self.signInTextFieldView.center = self.initialSignInFormCenter
             self.signInButtonView.center = self.initialSignInButtonCenter
         })
+        
+        if emailTextField.text.isEmpty || emailTextField.text == "Email" {
+            emailAlert.show()
+        } else if passwordTextField.text.isEmpty || passwordTextField.text == "Password" {
+            passwordAlert.show()
+        } else if passwordTextField.text != "password" && emailTextField.text != "pat@gmail.com" {
+            loadingAlert.show()
+            delay(2) {
+                loadingAlert.dismissWithClickedButtonIndex(0, animated: true)
+                signInAlert.show()
+            }
+        } else {
+            loadingAlert.show()
+            delay(2) {
+                loadingAlert.dismissWithClickedButtonIndex(0, animated: true)
+                self.performSegueWithIdentifier("loginSegue", sender: nil)
+            }
+        }
     }
     
     func animateUpSignInForm() {
